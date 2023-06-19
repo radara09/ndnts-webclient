2f446c9f0e58f681fa68637a706e761d9707d27f
radara09 <rahmathfir@gmail.com>
2023-06-19 16:17:04 +0700

    code main.js yang baru ada content-nya

---
+++ src/main.js
@@ -0,0 +1,60 @@
+import { connectToNetwork, connectToRouter } from "@ndn/autoconfig";
+import { Endpoint } from "@ndn/endpoint";
+import { AltUri, Interest, Name } from "@ndn/packet";
+import { WsTransport } from "@ndn/ws-transport";
+
+async function ping(evt) {
+  evt.preventDefault();
+  // Disable the submit button during function execution.
+  const $button = document.querySelector("#app_button");
+  $button.disabled = true;
+
+  try {
+    // Construct the name prefix <user-input>+/ping
+    const prefix = new Name(document.querySelector("#app_prefix").value);
+    const $log = document.querySelector("#app_log");
+    $log.textContent = `ping ${AltUri.ofName(prefix)}\n`;
+
+    const endpoint = new Endpoint();
+    // Generate a random number as initial sequence number.
+    let seqNum = Math.trunc(Math.random() * 1e8);
+    for (let i = 0; i < 4; ++i) {
+      ++seqNum;
+      // Construct an Interest with prefix + seqNum.
+      const interest = new Interest(prefix,
+        Interest.MustBeFresh, Interest.Lifetime(1000));
+      const t0 = Date.now();
+      try {
+        // Retrieve Data and compute round-trip time.
+        const data = await endpoint.consume(interest);
+        const rtt = Date.now() - t0;
+        const dataContent = data.content;
+        //console.log(dataContent);
+        $log.textContent += `\n${AltUri.ofName(data.name)} rtt=${rtt}ms content=${String.fromCharCode(...dataContent)}`;
+      } catch(err) {
+        // Report Data retrieval error.
+        $log.textContent += `\n${AltUri.ofName(interest.name)} ${err}`;
+      }
+
+      // Delay 500ms before sending the next Interest.
+      await new Promise((r) => setTimeout(r, 500));
+    }
+  } finally {
+    // Re-enable the submit button.
+    $button.disabled = false;
+  }
+}
+
+async function main() {
+  // Connect to the global NDN network in one line.
+  // This function queries the NDN-FCH service, and connects to the nearest router.
+  //await connectToRouter("wss://192.168.56.106:9696/ws/", {});
+  //await WsTransport.createFace({}, "wss://testbed-ndn-rg.stei.itb.ac.id/ws/");
+  await WsTransport.createFace({}, "ws://192.168.56.106:9696/ws/");
+
+  // Enable the form after connection was successful.
+  document.querySelector("#app_button").disabled = false;
+  document.querySelector("#app_form").addEventListener("submit", ping);
+}
+
+window.addEventListener("load", main);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("node:path");
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  performance: {
+    hints: false,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    allowedHosts: "all",
+    port: 3333,
+  },
+};
