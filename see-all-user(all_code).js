ddb91866139037069f470458f09cb3351f756eb6
radara09 <75063520+radara09@users.noreply.github.com>
2023-07-26 15:32:40 +0700

    Update see-all-user(all_code).js

--- see-all-user(all_code).js
+++ see-all-user(all_code).js
@@ -159,158 +159,47 @@ async function seeAll(evt) { //membuat fungsi async
   const t0 = Date.now();
   const data = await endpoint.consume(interest);
   const rtt = Date.now() - t0;
-  const dataContent = data.content;
-  
-  //$log.textContent += `content= ${String.fromCharCode(...dataContent)}\n`; //print data respon
-    console.log(dataContent)
-    console.log(`${rtt} ms`);
+
+const dataContent = data.content;
+console.log(dataContent)
+console.log(`${rtt} ms`);
+
+const dataBaru = decoder.decode(dataContent);
+console.log(dataBaru);
+const jsonData = JSON.parse(dataBaru);
+console.log(jsonData);
+
+
+// Ambil elemen dengan ID dataContainer untuk menampilkan data
+const dataContainer = document.getElementById('dataContainer');
+
+// Loop melalui data JSON dan tampilkan di dalam div
+Object.entries(jsonData).forEach([, pasien] => {
+// Buat elemen untuk menampilkan data pasien
+const pasienElement = document.createElement('div');
+pasienElement.className = 'pasien';
+
+// Tampilkan data pasien di dalam elemen yang telah dibuat
+pasienElement.innerHTML = `
+    <h2>Nama: ${pasien.Nama}</h2>
+    <p>Umur: ${pasien.Umur}</p>
+    <p>Jenis Kelamin: ${pasien.Sex}</p>
+    <p>No.Pasien: ${pasien.Diagnosis}</p>
+    <p>: ${pasien.Sex}</p>
     
-    const dataBaru = decoder.decode(dataContent);
-    console.log(dataBaru);
-    const jsonData = JSON.parse(dataBaru);
-    console.log(jsonData);
+`;
 
-    
-    // Ambil elemen dengan ID dataContainer untuk menampilkan data
-    const dataContainer = document.getElementById('dataContainer');
+// Tambahkan elemen pasien ke dalam kontainer
+dataContainer.appendChild(pasienElement);
+});
 
-    // Loop melalui data JSON dan tampilkan di dalam div
-    data.forEach(pasien => {
-    // Buat elemen untuk menampilkan data pasien
-    const pasienElement = document.createElement('div');
-    pasienElement.className = 'pasien';
-
-    // Tampilkan data pasien di dalam elemen yang telah dibuat
-    pasienElement.innerHTML = `
-        <h2>Nama: ${pasien.Nama}</h2>
-        <p>Umur: ${pasien.Umur}</p>
-        <p>Jenis Kelamin: ${pasien.Sex}</p>
-        <p>No.Pasien: ${pasien.Diagnosis}</p>
-        <p>: ${pasien.Sex}</p>
-        
-    `;
-
-    // Tambahkan elemen pasien ke dalam kontainer
-    dataContainer.appendChild(pasienElement);
-    });
-
-    }
+}
 
 async function main() {
   const face = await WsTransport.createFace({}, "wss://hmbe.ndntel-u.my.id:9696");
   face.addRoute(new Name("/"));
   // Enable the form after connection was successful.
   document.querySelector("#seeAllButton").addEventListener("click", seeAll);
-}
-window.addEventListener("load", main);
-
-
-  // Fungsi untuk menampilkan data pasien
-  function renderData(data) {
-    const dataContainer = document.getElementById("dataContainer");
-    dataContainer.innerHTML = ""; // Reset konten sebelum menampilkan data baru
-  
-    // Tampilkan data pada halaman
-    if (data.length > 0) {
-      data.forEach((pasienData) => {
-        const pasienHTML = `
-          <div>
-            <strong>Nomor Pasien:</strong> ${pasienData.nomor_pasien}<br>
-            <strong>Nama:</strong> ${pasienData.nama}<br>
-            <strong>Umur:</strong> ${pasienData.umur}<br>
-          </div>
-          <hr>
-        `;
-        dataContainer.innerHTML += pasienHTML;
-      });
-    } else {
-      dataContainer.innerHTML = "<p>Tidak ada data pasien.</p>";
-    }
-  }
-  
-  // Panggil fungsi renderData() untuk menampilkan data saat halaman dimuat
-  renderData(dataPasien);
-  
-  // Tambahkan event listener untuk tombol "See All" agar memanggil fungsi seeAllData() saat diklik
-  const seeAllButton = document.getElementById("seeAllButton");
-  seeAllButton.addEventListener("click", seeAllData);
-  
-  // Fungsi untuk menampilkan semua data saat tombol "See All" diklik
-  function seeAllData() {
-    // Di sini, Anda bisa mengambil data dari server melalui API atau sumber lainnya
-    // dan kemudian memanggil fungsi renderData() dengan data yang diambil dari server
-    // Contoh menggunakan data simulasi:
-    renderData(dataPasien);
-  }
-  
-
-*/
-
-/*Tes Lagi
-
-async function seeOne(evt) {
-  evt.preventDefault();
-  const prefix = new Name("/data/getuser");
-  const app = document.querySelector("#app_name").value;
-
-  const endpoint = new Endpoint();
-  const encoder = new TextEncoder();
-  const interest = new Interest();
-  const decoder = new TextDecoder();
-
-  interest.name = prefix;
-  interest.mustBeFresh = true;
-  interest.lifetime = 1000;
-  interest.appParameters = encoder.encode(app);
-  await interest.updateParamsDigest();
-
-  const t0 = Date.now();
-  const data = await endpoint.consume(interest);
-  const rtt = Date.now() - t0;
-  const dataContent = data.content;
-
-  const dataBaru = decoder.decode(dataContent);
-  console.log(dataBaru);
-  const jsonData = JSON.parse(dataBaru);
-  console.log(jsonData);
-
-  // Memanggil fungsi untuk menampilkan data berdasarkan id yang diberikan
-  const idToFind = 2; // Contoh id yang ingin dicari, sesuaikan dengan skenario Anda
-  findAndDisplayData(jsonData, idToFind);
-}
-
-// Fungsi untuk mencari data berdasarkan id dan menampilkannya
-function findAndDisplayData(jsonData, idToFind) {
-  const dataFound = jsonData[idToFind];
-
-  // Cek apakah data ditemukan atau tidak
-  if (dataFound) {
-    const dataContainer = document.getElementById("dataContainer");
-
-    // Buat elemen-elemen HTML untuk menampilkan data
-    const nameElement = document.createElement("p");
-    nameElement.textContent = `Name: ${dataFound.name}`;
-
-    const ageElement = document.createElement("p");
-    ageElement.textContent = `Age: ${dataFound.age}`;
-
-    const idElement = document.createElement("p");
-    idElement.textContent = `ID: ${dataFound.id}`;
-
-    // Menambahkan elemen-elemen ke dalam div "dataContainer"
-    dataContainer.appendChild(nameElement);
-    dataContainer.appendChild(ageElement);
-    dataContainer.appendChild(idElement);
-  } else {
-    console.log("Data not found!");
-  }
-}
-
-async function main() {
-  const face = await WsTransport.createFace({}, "wss://hmbe.ndntel-u.my.id:9696");
-  face.addRoute(new Name("/"));
-  // Enable the form after connection was successful.
-  document.querySelector("#app_form").addEventListener("submit", seeOne);
 }
 window.addEventListener("load", main);
 
